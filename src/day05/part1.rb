de40d3053d6852548a67b7d7db05c2ffec9448c9
Megatank58 <tanumegatank@gmail.com>
2022-12-05 13:15:50 +0000

    chore: golf day 5

--- src/day05/part1.rb
+++ src/day05/part1.rb
@@ -2,13 +2,13 @@ i=gets(nil).split("\n\n")
 b=i[0].split("\n")
 m=i[1].split("\n")
 r=b.pop.split.last.to_i
-a=b.map{|c| c.split(/\[?\]?/)}
+a=b.map{|c|c.split(/\[?\]?/)}
 f=[]
 s=""
 c=0
-a=a.map{|d| d.map{|e| if e==" " then c+=1; if c==3 then c=-1; "[0]" else nil end else c=0; e end}.map{|e|e=="" ? nil : e}.compact}
+a=a.map{|d|d.map{|e|e==" " ? (c+=1;c==3 ? (c=-1;"[0]"):nil):(c=0;e)}.map{|e|e=="" ? nil:e}.compact}
 r.times{|i|f[i]=a.map{|r|r[i]}}
-f=f.map{|k| k.map{|e|e=="[0]" ? nil:e}.compact}
-m.each{|k| k=k.split; k[1].to_i.times{Zf[k[5].to_i-1].prepend(f[k[3].to_i-1].shift)}}
-f.each{|k| s+=k[0]}
+f=f.map{|k|k.map{|e|e=="[0]" ? nil:e}.compact}
+m.each{|k|k=k.split;k[1].to_i.times{f[k[5].to_i-1].prepend(f[k[3].to_i-1].shift)}}
+f.each{|k|s+=k[0]}
 p s
--- src/day05/part2.rb
+++ src/day05/part2.rb
@@ -6,9 +6,9 @@ a=b.map{|c| c.split(/\[?\]?/)}
 f=[]
 s=""
 c=0
-a=a.map{|d| d.map{|e| if e==" " then c+=1; if c==3 then c=-1; "[0]" else nil end else c=0; e end}.map{|e|e=="" ? nil : e}.compact}
+a=a.map{|d|d.map{|e|e==" " ? (c+=1;c==3 ? (c=-1;"[0]"):nil):(c=0;e)}.map{|e|e=="" ? nil:e}.compact}
 r.times{|i|f[i]=a.map{|r|r[i]}}
-f=f.map{|k| k.map{|e|e=="[0]" ? nil:e}.compact}
-m.each{|k| k=k.split; j=f[k[3].to_i-1][..k[1].to_i-1]; j.reverse.map{|e|f[k[5].to_i-1].prepend(e)}; j.size.times{f[k[3].to_i-1].shift}}
-f.each{|k| s+=k[0]}
+f=f.map{|k|k.map{|e|e=="[0]" ? nil:e}.compact}
+m.each{|k|k=k.split;j=f[k[3].to_i-1][..k[1].to_i-1];j.reverse.map{|e|f[k[5].to_i-1].prepend(e)};j.size.times{f[k[3].to_i-1].shift}}
+f.each{|k|s+=k[0]}
 p s
